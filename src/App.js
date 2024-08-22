4927960f2334cb9457914ecb367b70bf499007d0
Karuna Subedi <karyanna7@gmail.com>
2024-08-22 19:39:03 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AllBlogs from "./pages/AllBlogs";
+import SingleBlog from "./pages/SingleBlog";
+import CreateBlog from "./pages/CreateBlog";
+import EditBlog from "./pages/EditBlog";
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AllBlogs></AllBlogs>} />
+        <Route path="/single-blog" element={<SingleBlog></SingleBlog>} />
+        <Route path="/create-blog" element={<CreateBlog></CreateBlog>} />
+        <Route path="/edit-blog" element={<EditBlog></EditBlog>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AllBlogs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css";
+import Navbar from "../components/Navbar";
+
+const AllBlogs = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default AllBlogs;
